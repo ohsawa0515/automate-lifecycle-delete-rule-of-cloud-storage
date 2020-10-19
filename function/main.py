86fe6a0be2fb8351a9139f56ddaa6d3971f5b735
Shuichi Ohsawa <ohsawa0515@gmail.com>
2020-10-19 14:33:37 +0900

    output bucket name

--- function/main.py
+++ function/main.py
@@ -14,5 +14,6 @@ def main_handler(event, context):
     pubsub_message = json.loads(base64.b64decode(event['data']).decode('utf-8'))
     resource_name = pubsub_message[u'protoPayload'][u'resourceName']
     bucket_name = resource_name.split('/')[3]
+    print("Bucket: %s" % bucket_name)
     enable_bucket_lifecycle(bucket_name)
 
