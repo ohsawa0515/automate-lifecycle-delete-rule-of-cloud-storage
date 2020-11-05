818d57eeb2c82b19b6b54e8267102f69a2c77268
Shuichi Ohsawa <ohsawa0515@gmail.com>
2020-11-05 06:24:57 +0000

    change print to logging

--- function/main.py
+++ function/main.py
@@ -3,10 +3,18 @@ import json
 import os
 import sys
 import re
+from logging import getLogger, StreamHandler, INFO
 from google.cloud import storage
 
 age = os.environ.get('LIFECYCLE_EXPIRE')
 ignorePatterns = os.environ.get('IGNORE_PATTERNS')
+
+logger = getLogger(__name__)
+handler = StreamHandler()
+handler.setLevel(INFO)
+logger.setLevel(INFO)
+logger.addHandler(handler)
+logger.propagate = False
 
 # Add lifecycle rule which deletes object after 365 days
 def enable_bucket_lifecycle(bucket_name):
@@ -14,19 +22,20 @@ def enable_bucket_lifecycle(bucket_name):
     bucket = client.get_bucket(bucket_name)
     bucket.add_lifecycle_delete_rule(age=age)
     bucket.patch()
+    logger.info("Lifecycle addition is complete.")
 
 def main_handler(event, context):
     pubsub_message = json.loads(base64.b64decode(event['data']).decode('utf-8'))
     resource_name = pubsub_message[u'protoPayload'][u'resourceName']
     bucket_name = resource_name.split('/')[3]
-    print("Bucket: %s" % bucket_name)
+    logger.info("Bucket: %s" % bucket_name)
 
     for ignorePattern in ignorePatterns.split('###'):
         try:
             if re.match(ignorePattern, bucket_name):
-                print("Since it is included in ignorePattern '%s', it does not set the life cycle." % ignorePattern)
+                logger.info("Since it is included in ignorePattern '%s', it does not set the life cycle." % ignorePattern)
                 return
         except re.error as regex_error:
-            print("The grammar expression '%s' has an error : %s" % (ignorePattern, regex_error))
+            logger.warn("The grammar expression '%s' has an error : %s" % (ignorePattern, regex_error))
 
     enable_bucket_lifecycle(bucket_name)
